f8cc7cd2dd18bc1db0f6c1d02d9c64d531750386
krisha <krisharelle@gmail.com>
2025-04-07 15:08:20 -0400

    pushing practicum 8

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
 set -u
-: "$REGISTRY_URL"
-: "$VERSION"
+
+# Provide default values if not already set
+: "${REGISTRY_URL:=practicum7krisha.azurecr.io}"
+: "${VERSION:=v1.0.0}"
 
 envsubst < ./deploy.yaml | kubectl apply -f -
+bash deploy.sh
